063ed03dd3640080b32c1d719ae7414893a92d05
Oliver Lee <oliverzlee@gmail.com>
2020-12-06 12:23:29 +0100

    Define nullbuf for disabled logging

--- log.h
+++ log.h
@@ -11,6 +11,15 @@
 
 namespace output_streambuf {
 
+struct nullbuf : std::streambuf {
+    using base_type = std::streambuf;
+    using char_type = typename base_type::char_type;
+    using int_type = typename base_type::int_type;
+
+  protected:
+    auto overflow(int_type ch) -> int_type override { return ch; }
+};
+
 template <std::size_t N>
 struct arraybuf : std::streambuf {
     using base_type = std::streambuf;
@@ -161,7 +170,14 @@ struct info_t {
 
 template <class LogTag, class T>
 auto operator<<(LogTag, T&& t) -> std::ostream& {
+#if !defined(DISABLE_LOGGING)
     return std::decay_t<LogTag>::get_impl() << std::forward<T>(t);
+#else
+    (void)t;
+    static auto buf = osbuf::nullbuf{};
+    static auto os = std::ostream{&buf};
+    return os;
+#endif
 }
 
 } // namespace detail
